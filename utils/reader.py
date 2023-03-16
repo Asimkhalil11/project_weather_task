76fedb27b0203c338531cd0c1a288d4fa91de3a8
asim khalil <aximmak11@gmail.com>
2023-03-16 16:25:17 +0500

    task1

---
+++ utils/reader.py
@@ -0,0 +1,10 @@
+def read_file():
+    with open("/home/asim/Desktop/ISDP/Sir Hammad/project_weather_task/files/f1.csv") as f:
+        contents = f.read()
+
+        rows = []
+        for row in contents.split("\n")[1:-1]:
+            rows.append(row)
+        # print(rows)
+
+    return rows
