e69ffab9491ac4884a54121ee9b9b6ceb81ee055
asim khalil <aximmak11@gmail.com>
2023-03-16 16:56:35 +0500

    delete files

--- task/task1.py
+++ task/task1.py
@@ -3,7 +3,6 @@ from utils.reader import read_file
 files_values = read_file()
 
 for row in files_values:
-    # print(row)
     date, max_temp, min_temp = row.split(",")[0:2]+ row.split(",")[3:4]
     max_min_diff = int(max_temp) - int(min_temp)
 
