5ee9ded6ba434a45e77b77ae0b22c579982ca54b
asim khalil <aximmak11@gmail.com>
2023-03-22 17:25:41 +0500

    task6

--- task/task6.py
+++ task/task6.py
@@ -4,10 +4,10 @@ from datetime import datetime
 file2_content = read_file("/home/asim/Desktop/ISDP/Sir Hammad/project_weather_task/files/f2.csv")
 
 for row in file2_content:
-    date = row.split(",")[1]
+    event_date = row.split(",")[1]
     event = row.split(",")[22]
-    if event in ["Thunderstorm"]:
-        date_object = datetime.strptime(date, "%Y-%m-%d")
+    if event == "Thunderstorm":
+        date_object = datetime.strptime(event_date, "%Y-%m-%d")
         week_day = date_object.strftime("%A")
         print(f"{event} {week_day}")
 
